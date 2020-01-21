7539caacdde6d8e6e4239465e9fa9468be789bf5
KrisAnTis-Group <Krisantis@mail.ru>
2020-01-21 10:25:56 -0800

    EDA  reorg + hist

--- EDA.py
+++ EDA.py
@@ -8,64 +8,51 @@
 # 7. определить колличество уникальных классов (меток)
 # 8. построить гистограмму распределения меток в обучающей и тестовой выборках
 # 9. оценить отношение размера словаря к размеру текста
-# 10. оценить процентное соотношение ненормативной лексики в словаре к размеру словаря
-# 11. определить тональности текстов по оценки агрессивности
 
 import numpy as np
 
 import matplotlib.pyplot as plt
 
-import data
-from data import get_DataSet_on_numpy, tokenize_corpus, build_vocabulary
+def print_count_texts_of_DS (train_sourse, test_sourse):
+    print('Количество обучающих текстов',len(train_sourse['data']))
+    print('Количество тестовых текстов',len(test_sourse['data']))
+    print('Количество уникальных меток', len(set(train_sourse['target'])))
+    print()
 
-# импорт данных из дата сета *.csv
-train_sourse = get_DataSet_on_numpy(subset = "train")
-test_sourse = get_DataSet_on_numpy(subset = "test")
+    print('Количество обучающих меток',len(np.unique(train_sourse['target'])))
+    print('Количество тестовых меток',len(np.unique(test_sourse['target'])))
+    print()
 
-print('Количество обучающих текстов',len(train_sourse['data']))
-print('Количество тестовых текстов',len(test_sourse['data']))
-print()
-print(train_sourse['data'][0].strip())
-print()
-print('Метка: ',train_sourse['target'][0])
-
-print('Количество обучающих меток',len(np.unique(train_sourse['target'])))
-print('Количество тестовых меток',len(np.unique(test_sourse['target'])))
-
-# разбиение текстов на токены: буквы, цифры больше 4 символов
-# получаем две матрицы: строки(тексты) * столбцы (признаки)
-train_tokinized = tokenize_corpus(train_sourse['data'])
-test_tokinized = tokenize_corpus(test_sourse['data'])
-
-print(' '.join(train_tokinized[0]))
-
-# строим словарь: слова -> цифры (нумеруем токены)
-MAX_DF = 0.8
-MIN_COUNT = 5
-
-vocabulary, word_doc_freq = build_vocabulary (train_tokinized, max_doc_freq = MAX_DF, min_count = MIN_COUNT)
-
-UNIQUE_WORDS_N = len(vocabulary)
-print('Количество уникальных токенов', UNIQUE_WORDS_N)
-print(list(vocabulary.items())[:int(UNIQUE_WORDS_N/10)])
-
-plt.hist(word_doc_freq, bins=10)
-plt.title('Распределение относительных частот слов')
-plt.yscale('log')
-plt.show()
-
-UNIQUE_LABELS_N = len(set(train_sourse['target']))
-print('Количество уникальных меток', UNIQUE_LABELS_N)
+    print('Пример входного набора:\n',train_sourse['data'][0].strip())
+    print('Метка:\n',train_sourse['target'][0])
 
 
-plt.hist(train_sourse['target'], bins=np.arange(0, UNIQUE_LABELS_N+1))
-plt.yscale('log')
-plt.xscale('linear')
-plt.title('Распределение меток в обучающей выборке')
-plt.show()
+def print_texr_example(train_tokinized):
+    print(' '.join(train_tokinized))
 
-plt.hist(test_sourse['target'], bins=np.arange(0, UNIQUE_LABELS_N+1))
-plt.title('Распределение меток в тестовой выборке')
-plt.yscale('log')
-plt.xscale('linear')
-plt.show()
+
+def print_unique_tokin (vocabulary):
+    UNIQUE_WORDS_N = len(vocabulary)
+    print('Количество уникальных токенов', UNIQUE_WORDS_N)
+    print(list(vocabulary.keys())[:int(UNIQUE_WORDS_N/10)])
+
+def show_hist_word_frequency_dist(word_doc_freq):
+    plt.hist(word_doc_freq, bins=10)
+    plt.title('Распределение относительных частот слов')
+    plt.yscale('log')
+    plt.show()
+
+def show_hist_target_dist(train_sourse, test_sourse):
+    UNIQUE_LABELS_N = len(set(train_sourse['target']))
+    plt.hist([train_sourse['target'],test_sourse['target']], bins=np.arange(0, UNIQUE_LABELS_N+1), alpha=0.8)    
+    plt.yscale('log')
+    plt.xscale('linear')
+    plt.title('Распределение меток в выборке')
+    plt.show()
+
+def spamming_of_text(test_sourse,UNIQUE_WORDS_N):
+    countWordInCorpus = 0
+    for tx in test_sourse['target']:
+        countWordInCorpus += len(tx)
+    print('Количество употреблённых слов: ', countWordInCorpus)
+    print('В среднем, любое слово повторяется в корпусе: ', countWordInCorpus/UNIQUE_WORDS_N," раз")
